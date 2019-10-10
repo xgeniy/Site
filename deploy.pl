03eca8870eaf74155f84b23023903f18bc18766f
Xgeniy <xoxlov-2017@inbox.ru>
2019-10-10 12:26:05 +0300

    Commit6

--- deploy.pl
+++ deploy.pl
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Content-type: text/plain\n";
 echo '';
-cd /var/www/domains/ваш_сайт/ || exit > /dev/null
+cd /var/www/domains/e.qzo.su/ || exit > /dev/null
 git reset --hard > /dev/null
 git pull > /dev/null
-chmod -R 755 /var/www/domains/e.qzo.su > /dev/null
+chmod -R 755 /var/www/domains/e.qzo.su \> /dev/null
