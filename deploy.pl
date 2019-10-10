cee3c0531f2b30a7efe0d0a49e0887d1d8d27dbc
Xgeniy <xoxlov-2017@inbox.ru>
2019-10-10 12:23:32 +0300

    Commit5

--- deploy.pl
+++ deploy.pl
@@ -4,4 +4,4 @@ echo '';
 cd /var/www/domains/ваш_сайт/ || exit > /dev/null
 git reset --hard > /dev/null
 git pull > /dev/null
-chmod -R 755 /var/www/domains/ваш_сайт > /dev/null
+chmod -R 755 /var/www/domains/e.qzo.su > /dev/null
